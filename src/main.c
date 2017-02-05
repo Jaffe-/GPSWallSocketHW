6a418e91ef6c09ed0a8c4d2763c32355aab3fab2
Johan Fjeldtvedt <jaffe1@gmail.com>
2017-02-05 14:17:34 +0100

    add cooperative task handler

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <avr/io.h>
-#include "display.h"
+#include "task/task.h"
+
+void blinky() {
+    PORTB ^= 1 << 5;
+}
 
 int main(void)
 {
-    while(1);
+    task_setup();
+    DDRB |= 1 << 5;
+
+    task_manager();
 }
