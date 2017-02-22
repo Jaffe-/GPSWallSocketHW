b50bac1c368824f7c42c982ed794b75341a0b355
Johan Fjeldtvedt <jaffe1@gmail.com>
2017-02-22 14:20:26 +0000

    add functions for handling protocol

--- node/libs/protocol.cpp
+++ node/libs/protocol.cpp
@@ -1,3 +1,72 @@
 #include "protocol.h"
 
 /* Common protocol functions */
+
+void create_msg_on(uint8_t *buffer) {
+    buf[0] = ON;
+}
+
+void create_msg_off(uint8_t *buffer) {
+    buf[0] = OFF;
+}
+
+void create_msg_config(uint8_t *buffer, uint32_t address) {
+    buf[0] = CONFIG;
+    *(static_cast<uint32_t*>(&buffer[1])) = address;
+}
+
+void create_msg_init_config(uint8_t *buffer) {
+    buf[0] = INIT_CONFIG;
+}
+
+void create_msg_status(uint8_t *buffer, RelayState relay_state, ControlState control_state, float current) {
+    buf[0] = STATUS;
+    buf[1] = relay_state;
+    buf[2] = control_state;
+    *(static_cast<float*>(&buffer[3])) = current;
+}
+
+void decode_msg_config(uint8_t *buffer, uint32_t *address) {
+    *address = *(static_cast<uint32_t*>(&buffer[1]));
+}
+
+void decode_msg_status(uint8_t *buffer, RelayState *relay_state, ControlState *control_state, float *current) {
+    *relay_state = buf[1];
+    *control_state = buf[2];
+    *current = *(static_cast<float*>(&buffer[3]));
+}
+
+bool verify_msg(uint8_t *buffer) {
+    MessageType msg_type = get_msg_type(buffer);
+    bool found = false;
+    for (type : { MessageType::ACK,
+                  MessageType::ON,
+                  MessageType::OFF,
+                  MessageType::CONFIG,
+                  MessageType::INIT_CONFIG,
+                  MessageType::STATUS }) {
+        if (type == msg_type) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        return false;
+
+    if (state == MessageType::STATUS) {
+        RelayState rs;
+        ControlState cs;
+        float current;
+        decode_msg_status(buffer, &rs, &cs, &value);
+
+        if (!(rs == RelayState::ON || rs == RelayState::OFF))
+            return false;
+
+        if (!(cs == ControlState::ON || cs == ControlState::OFF || cs == ControlState::GEO))
+            return false;
+    }
+
+    return true;
+    }
+}
--- node/libs/protocol.h
+++ node/libs/protocol.h
@@ -1,3 +1,33 @@
 #pragma once
 
 /* Header file for common protocol functions */
+
+enum class MessageType : uint8_t {
+    ACK = 0xFF,
+    ON = 0xF0,
+    OFF = 0x0F,
+    CONFIG = 0xA0,
+    INIT_CONFIG = 0x0A,
+    STATUS = 0x55,
+};
+
+enum class RelayState : uint8_t {
+    ON = 0xF0, OFF = 0x0F
+};
+
+enum class ControlState : uint8_t {
+    ON = 0xF0, OFF = 0x0F, GEO = 0xFF
+};
+
+MessageType get_msg_type(uint8_t *buffer);
+
+void create_msg_on(uint8_t *buffer);
+void create_msg_off(uint8_t *buffer);
+void create_msg_config(uint8_t *buffer, uint32_t address);
+void create_msg_init_config(uint8_t *buffer);
+void create_msg_status(uint8_t *buffer, RelayState relay_state, ControlState control_state, float current);
+
+void decode_msg_config(uint8_t *buffer, uint32_t *address);
+void decode_msg_status(uint8_t *buffer, RelayState *relay_state, ControlState *control_state, float *current);
+
+bool verify_msg(uint8_t *buffer);
