a6d4f0480ed3b9f7e43ece5216259635e12d2f15
Johan Fjeldtvedt <jaffe1@gmail.com>
2017-02-22 21:23:33 +0000

    fix protocol.h/cpp
    Quite a few compile bugs that I didn't see. Fixed them and also
    made templated functions for serializing and deserializing. Using
    these templates is safer than hand-calculating index displacements
    etc.
    
    Also changed enum to enum class because of name collisions between
    different enums. It's better to use enum class anyway since it makes
    the code cleaner when enums have a defined scope, i.e.
    MessageType::ON instead of just ON.

--- node/libs/protocol.cpp
+++ node/libs/protocol.cpp
@@ -1,50 +1,84 @@
 #include "protocol.h"
+#include <initializer_list>
 
 /* Common protocol functions */
 
+#define FIELDS_START 5    // 1 byte type + 4 byte address
+
+static uint32_t my_address;
+
+void protocol_set_my_address(uint32_t address) {
+    my_address = address;
+}
+
+
+void serialize(uint8_t *buffer) {};
+
+template <typename T, typename... Rest>
+void serialize(uint8_t *buffer, T first, Rest... rest) {
+    *(reinterpret_cast<T*>(buffer)) = first;
+    serialize(buffer + sizeof(T), rest...);
+}
+
 void create_msg_on(uint8_t *buffer) {
-    buf[0] = ON;
+    serialize(buffer, MessageType::ON, my_address);
 }
 
 void create_msg_off(uint8_t *buffer) {
-    buf[0] = OFF;
+    serialize(buffer, MessageType::OFF, my_address);
 }
 
-void create_msg_config(uint8_t *buffer, uint32_t address) {
-    buf[0] = CONFIG;
-    *(static_cast<uint32_t*>(&buffer[1])) = address;
+void create_msg_config(uint8_t *buffer, uint32_t new_address) {
+    serialize(buffer, MessageType::CONFIG, my_address, new_address);
 }
 
 void create_msg_init_config(uint8_t *buffer) {
-    buf[0] = INIT_CONFIG;
+    serialize(buffer, MessageType::INIT_CONFIG, my_address);
 }
 
 void create_msg_status(uint8_t *buffer, RelayState relay_state, ControlState control_state, float current) {
-    buf[0] = STATUS;
-    buf[1] = relay_state;
-    buf[2] = control_state;
-    *(static_cast<float*>(&buffer[3])) = current;
+    serialize(buffer, MessageType::STATUS, my_address, relay_state, control_state, current);
 }
 
-void decode_msg_config(uint8_t *buffer, uint32_t *address) {
-    *address = *(static_cast<uint32_t*>(&buffer[1]));
+
+/* Decode functions */
+
+MessageType get_msg_type(uint8_t *buffer) {
+    return *(reinterpret_cast<MessageType*>(buffer[0]));
+}
+
+uint32_t get_msg_address(uint8_t *buffer) {
+    return *(reinterpret_cast<uint32_t*>(buffer[1]));
+}
+
+void deserialize(uint8_t *buffer) {};
+
+template <typename T, typename... Rest>
+void deserialize(uint8_t *buffer, T* first, Rest*... rest) {
+    *first = *(reinterpret_cast<T*>(&buffer[FIELDS_START]));
+    deserialize(&buffer[FIELDS_START + sizeof(T)], rest...);
+}
+
+void decode_msg_config(uint8_t *buffer, uint32_t *new_address) {
+    deserialize(buffer, new_address);
 }
 
 void decode_msg_status(uint8_t *buffer, RelayState *relay_state, ControlState *control_state, float *current) {
-    *relay_state = buf[1];
-    *control_state = buf[2];
-    *current = *(static_cast<float*>(&buffer[3]));
+    deserialize(buffer, relay_state, control_state, current);
 }
+
+
+/* Verification that message is OK */
 
 bool verify_msg(uint8_t *buffer) {
     MessageType msg_type = get_msg_type(buffer);
     bool found = false;
-    for (type : { MessageType::ACK,
-                  MessageType::ON,
-                  MessageType::OFF,
-                  MessageType::CONFIG,
-                  MessageType::INIT_CONFIG,
-                  MessageType::STATUS }) {
+    for (auto type : { MessageType::ACK,
+                       MessageType::ON,
+                       MessageType::OFF,
+                       MessageType::CONFIG,
+                       MessageType::INIT_CONFIG,
+                       MessageType::STATUS }) {
         if (type == msg_type) {
             found = true;
             break;
@@ -54,11 +88,11 @@ bool verify_msg(uint8_t *buffer) {
     if (!found)
         return false;
 
-    if (state == MessageType::STATUS) {
+    if (msg_type == MessageType::STATUS) {
         RelayState rs;
         ControlState cs;
         float current;
-        decode_msg_status(buffer, &rs, &cs, &value);
+        decode_msg_status(buffer, &rs, &cs, &current);
 
         if (!(rs == RelayState::ON || rs == RelayState::OFF))
             return false;
@@ -68,5 +102,4 @@ bool verify_msg(uint8_t *buffer) {
     }
 
     return true;
-    }
 }
--- node/libs/protocol.h
+++ node/libs/protocol.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 /* Header file for common protocol functions */
 
