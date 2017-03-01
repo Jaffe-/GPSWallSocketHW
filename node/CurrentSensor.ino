5886471db1251425e5cdb2826f16bdf884b56452
Alex-Danielsen <alexada@stud.ntnu.no>
2017-03-01 12:53:59 +0100

    Add Current Sensor

---
+++ node/CurrentSensor.ino
@@ -0,0 +1,18 @@
+
+//current sensor property variables
+int analogPort = 0;
+int NO_FIELD = 505;
+
+//current sensor Setup
+void currentSensor_setup() {
+  
+}
+
+//functions here
+void calibrate() {
+  //Assumes relay is turned off at startup
+  int NO_FIELD = analogRead(0);
+}
+
+
+
