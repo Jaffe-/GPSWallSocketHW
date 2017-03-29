67b0839c9a585984b06b2aadf3e46ac00db2916e
Alex-Danielsen <alexada@stud.ntnu.no>
2017-03-29 14:16:31 +0200

    Move configure and relay off of the same pin

--- node/pinout.h
+++ node/pinout.h
@@ -1,21 +1,22 @@
+
+//Analog Pins
 //Current Sensor
 #define CURRENT_SENSOR A0
 
 //LEDs
 
-
-//Relay
-#define RELAY_OUT 5
-
-//Switches
-#define SWITCH_INPUT_A 2
-#define SWITCH_INPUT_B 3
+//Digital Pins
+//#define                   1
+#define SWITCH_INPUT_A      2
+#define SWITCH_INPUT_B      3
+//#define                   4
+#define RELAY_OUT           5
+#define CONFIGRE_BUTTON_PIN 6
+// 7-12 used by radio
 
 //Radio
-#define INT_SENSE_PIN 3;
+//#define INT_SENSE_PIN 3;
 
-//Configure Button
-#define CONFIGRE_BUTTON_PIN 5
 
 //Constants
 #define SENSOR_RDG_PER_PACKET 5
